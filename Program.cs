f7b693350c1af6616fa140c8b3cba493d2ec04a4
tiagolael09-lang <tiagolael09@uni9.edu.br>
2025-09-02 00:46:35 +0000

    feat: add porjeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("E ai, Tiago!");
+
+Televisao tv = new Televisao();
+
+tv. Tamanho = 22.5f;
+
+Console.WriteLine($" A tv tem o tamanho {tv.Tamanho}"); 
---
+++ Televisao.cs
@@ -0,0 +1,10 @@
+public class Televisao
+{
+    public float Tamanho { get;set;}
+    public int Resolucao { get;set;}
+    public int Volume { get;set;}
+    public int Canal { get;set;}
+    public bool Estado { get;set;}
+    
+
+}
